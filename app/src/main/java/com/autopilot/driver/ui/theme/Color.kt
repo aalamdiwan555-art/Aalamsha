dbf97f8fc62108b99c580f1612884f8fdab32065
aalamdiwan555-art <aalamdiwan555@gmail.com>
2026-08-29 11:56:41 +0530

    Add root Android project file app/src/main/java/com/autopilot/driver/ui/theme/Color.kt

---
+++ app/src/main/java/com/autopilot/driver/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.autopilot.driver.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val AutopilotPrimary = Color(0xFF78E6D0)
+val AutopilotSecondary = Color(0xFFFFC86B)
+val AutopilotError = Color(0xFFFFA9B2)
+val AutopilotBackground = Color(0xFF081013)
+val AutopilotSurface = Color(0xFF102126)
+val AutopilotText = Color(0xFFE0EEF0)
+val AutopilotMuted = Color(0xFF789094)
+val AutopilotSuccess = Color(0xFF78E6D0)
+val Ink950 = Color(0xFF0B161A)
