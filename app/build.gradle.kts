bea33e780cf6a5a56f06c1613db58f08a0e5a550
aalamdiwan555-art <aalamdiwan555@gmail.com>
2026-08-29 12:28:36 +0530

    Fix Android build in app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.9.0")
     implementation("com.google.mlkit:text-recognition:16.0.1")
 
     testImplementation("junit:junit:4.13.2")
